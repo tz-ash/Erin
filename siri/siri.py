a44f0d731fe141bc0aa396d0592aba1641f3486d
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-08-01 17:20:49 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -82,7 +82,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("`Bruh... Uploading... Please Wait...`")
+    m.edit("`Kido... Uploading... Please Wait...`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
